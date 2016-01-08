c27a7b09aefc58e6ce283d03c3205b5fe97055fe
Jeff Sadler <Jeff Sadler>
2016-01-08 16:56:32 -0500

    does wu and combined semivariograms for each storm and all storms combined

--- semivariogram_cj_all_in_one.py
+++ semivariogram_cj_all_in_one.py
@@ -14,7 +14,7 @@ def plot_stations(z):
     xlabel('Easting [m]')
     ylabel('Northing [m]')
     title('Rain (in)')
-    fig.savefig("C:/Users/jeff_dsktp/Box Sync/Sadler_1stPaper/rainfallgoogle.png")
+    fig.savefig('C:/Users/jeff_dsktp/Box Sync/Sadler_1stPaper/rainfallgoogle.png')
 
 
 def SVh(P, h, bw):
@@ -98,71 +98,34 @@ def cvmodel(P, model, hs, bw):
     # calculate the optimal parameters
     param = opt(model, sv[0], sv[1], C0)
     # return a covariance function
-    covfct = lambda h, a=param: model(h, a, C0) #changed from original "C0 - model( h, a, C0 )
+    covfct = lambda h, a=param: model(h, a, C0) #changed from original 'C0 - model( h, a, C0 )
     return covfct
 
-def create_semivariogram(df, name, date_range):
-    print "doing data for {}".format(file_name)
-    i = 1
-    for date in date_range:
-        print "analyzing data for {}".format(str(date))
-        df_for_date = df[df['date'] == date]
-        if len(df_for_date) == 0:
-            continue
-        P = np.array(df_for_date[['x', 'y', 'rain']])
-        P = P[~np.isnan(P).any(axis=1)]
-        # bandwidth, plus or minus bw meters
-        pd = squareform(pdist(P[:, :2]))
-        bw = np.percentile(pd[np.nonzero(pd)], 10)
-        # lags in bw meter increments from zero to 10,000
-        hs = np.arange(0, 18000, bw)
-        sv = SV(P, hs, bw)
+def create_semivariogram(df, name):
+    print 'working on data for {}'.format(name)
+    P = np.array(df[['x', 'y', 'rain']])
+    P = P[~np.isnan(P).any(axis=1)]
+    # bandwidth, plus or minus bw meters
+    pd = squareform(pdist(P[:, :2]))
+    bw = np.percentile(pd[np.nonzero(pd)], 10)
+    # lags in bw meter increments from zero to 10,000
+    hs = np.arange(0, 18000, bw)
+    sv = SV(P, hs, bw)
+    sp = cvmodel(P, spherical, hs, bw)
+    plot(sv[0], sv[1], '.--', lw=0.8, ms=4)
+    nugget = sv[1][0]
+    model_results = [r + nugget for r in sp(sv[0])]
+    plot(sv[0], model_results, color='r', lw=0.8)
+    xlim(0, 18000)
+    # ylim(-1500,17500)
+    # figure(figsize=(7, 4))
+    title(name)
+    tick_params(labelsize=4, length=2)
+    grid( color='0.65')
 
-
-        sp = cvmodel(P, spherical, hs, bw)
-        subplot(10, 2, i)
-        plot(sv[0], sv[1], '.--', lw=0.8, ms=4)
-        nugget = sv[1][0]
-        model_results = [r + nugget for r in sp(sv[0])]
-        plot(sv[0], model_results, color="r", lw=0.8)
-        xlim(0, 16000)
-        # ylim(-1500,17500)
-        # figure(figsize=(7, 4))
-        if i == 19 or i == 20:
-            xlabel('Lag [m]', fontsize=4)
-        if i % 2 == 1:
-            ylabel('Semivariance', fontsize=4)
-        title((datetime.datetime.strptime(str(date), '%Y%m%d').strftime('%m/%d/%y')), fontsize=5)
-        tick_params(labelsize=4, length=2)
-        grid( color='0.65')
-
-        i += 1
     tight_layout(pad=0.05, w_pad=0.05, h_pad=0.05)
-    savefig('semivariogram_figure/semivariogram_model {}.png'.format(file_name), fmt='png', dpi=200, bbox_inches='tight')
+    savefig('semivariogram_figure/semivariogram_model_total {}.png'.format(name), fmt='png', dpi=200, bbox_inches='tight')
     close()
-
-
-
-date_range = [20130702,
-              20131009,
-              20140111,
-              20140213,
-              20140415,
-              20140425,
-              20140710,
-              20140818,
-              20140908,
-              20140909,
-              20140913,
-              20141126,
-              20141224,
-              20150414,
-              20150602,
-              20150624,
-              20150807,
-              20150820,
-              20150930,
-              20151002]
 
 df_list = []
 path = 'C:/Users/jeff_dsktp/Box Sync/Sadler_1stPaper/rainfall/data/'
@@ -174,7 +137,7 @@ for file_name in file_name_list:
 combined_df = pandas.DataFrame(columns=['x','y','rain'])
 for df in df_list:
     combined_df = combined_df.append(df['df'], ignore_index=True)
-df_list.append({'df':combined_df, 'name': 'combined'})
+df_list.append({'df':combined_df, 'name': 'combined'})    
 
 for df in df_list:
-    create_semivariogram(df['df'], df['name'], date_range)
+    create_semivariogram(df['df'], df['name'])
