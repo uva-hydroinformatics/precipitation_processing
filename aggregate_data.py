4a062eccb6edfb6bebc2e8dc22cc283938d4e2c0
Jeff Sadler <Jeff Sadler>
2016-02-19 15:03:33 -0500

    subsets hrsd data so it's only the relevant ones

--- aggregate_data.py
+++ aggregate_data.py
@@ -3,7 +3,7 @@
 # Email: jms3fb@virginia.edu
 
 import numpy as np
-import pandas
+import pandas as pd
 import pyodbc
 import datetime
 
@@ -21,15 +21,15 @@ def get_data_frame_from_table(table_name):
     SQL = 'SELECT * FROM {};'.format(table_name) # your query goes here
     rows = cur.execute(SQL).fetchall()
     a = np.array(rows)
-    df = pandas.DataFrame(a, columns=[i[0] for i in cur.description])
+    df = pd.DataFrame(a, columns=[i[0] for i in cur.description])
     cur.close()
     con.close()
     return df
 
 
 def make_incremental(df, date_range):
-    newdf = pandas.DataFrame()
-    non_cumulative_df = pandas.DataFrame()
+    newdf = pd.DataFrame()
+    non_cumulative_df = pd.DataFrame()
     for date in date_range:
         date_string = datetime.datetime.strptime(str(date), '%Y%m%d').strftime('%Y-%m-%d')
         df_date = df[date_string]
@@ -54,14 +54,27 @@ def make_incremental(df, date_range):
     return newdf
 
 
-def aggregate_time_steps(df, date):
+def aggregate_time_steps(df, date, time_step):
     # return a dataframe with the sum of the rainfall at a given point for a given time span
     date_string = datetime.datetime.strptime(str(date), '%Y%m%d').strftime('%Y-%m-%d')
     df_date = df[date_string]
     df_date = df_date.groupby(['x', 'y', 'site_name', 'src'])
-    df_date = df_date.resample('15T', how={'precip_mm': 'sum'})
+    df_date = df_date.resample(time_step, how={'precip_mm': 'sum'})
     df_date = df_date.reset_index(inplace=False)
     return df_date
+
+hrsd_stations_in_study_area = ["MMPS-171",
+                               "MMPS-185",
+                               "MMPS-163",
+                               "MMPS-255",
+                               "MMPS-146",
+                               "MMPS-004",
+                               "MMPS-256",
+                               "MMPS-140",
+                               "MMPS-160",
+                               "MMPS-144",
+                               "MMPS-036",
+                               "MMPS-093-2"]
 
 date_range = [
               20130702,
@@ -89,32 +102,33 @@ date_range = [
 # prepare the data by pulling from the database and making the datetime the index
 df_list = []
 df = get_data_frame_from_table('vabeach_reformat_mm')
-df['datetime'] = pandas.to_datetime(df['datetime'])
+df['datetime'] = pd.to_datetime(df['datetime'])
 vab_df = df.set_index('datetime')
 vab_df.insert(len(vab_df.columns), 'src', 'vab')
 
 df = get_data_frame_from_table('hrsd_obs_spatial')
-df['datetime'] = pandas.to_datetime(df['datetime'])
+df['datetime'] = pd.to_datetime(df['datetime'])
 hrsd_df = df.set_index('datetime')
 hrsd_df.insert(len(hrsd_df.columns), 'src', 'hrsd')
+hrsd_df = hrsd_df[hrsd_df.site_name.str.rstrip().isin(hrsd_stations_in_study_area)]
 
-df = get_data_frame_from_table('wu_observation_spatial')
-df['datetime'] = pandas.to_datetime(df['datetime'])
-df = df.set_index('datetime')
-inc_df = make_incremental(df, date_range)
-wu_df = inc_df.set_index('datetime')
+df = get_data_frame_from_table('wu_inc')
+df['datetime'] = pd.to_datetime(df['datetime'])
+wu_df = df.set_index('datetime')
 wu_df.insert(len(wu_df.columns), 'src', 'wu')
+wu_df.drop('site_name', axis=1, inplace=True)
+wu_df.rename(columns={'site_code':'site_name'}, inplace=True)
 
 df_list = [wu_df, hrsd_df, vab_df]
 
 #combine the dfs in the list together
-combined_df = pandas.DataFrame()
+combined_df = pd.DataFrame()
 for df in df_list:
     combined_df = combined_df.append(df)
 
-combined_agg_df = pandas.DataFrame()
+combined_agg_df = pd.DataFrame()
 for date in date_range:
-    indivi_df = aggregate_time_steps(combined_df, date)
+    indivi_df = aggregate_time_steps(combined_df, date, "D")
     combined_agg_df = combined_agg_df.append(indivi_df)
-
-combined_agg_df.to_csv("{}.csv".format("combined_aggregate_15_min"), mode='w', index=False)
+data_dir = "C:/Users/jeff_dsktp/Box Sync/Sadler_1stPaper/rainfall/data/"
+combined_agg_df.to_csv("{}{}.csv".format(data_dir, "combined_aggregate_filt"), mode='w', index=False)
